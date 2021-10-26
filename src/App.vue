68bb0955f0c71e184b442fe745c02b5b5f8cf116
libin <leebin66@126.com>
2021-10-26 11:34:28 +0800

    vite-plugin-components 插件按需加载组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  setup() {
+    let value = ref<number>(2);
+    return {
+      value,
+    };
+  },
+});
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <a-button type="primary">Primary Button</a-button>
+    <a-button>Default Button</a-button>
+    <a-button type="dashed">Dashed Button</a-button>
+    <a-button type="text">Text Button</a-button>
+    <a-button type="link">Link Button</a-button>
+    <a-rate v-model:value="value" />
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite' // 代码提示支持
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import ViteComponents, { AntDesignVueResolver } from 'vite-plugin-components'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    ViteComponents({
+      customComponentResolvers: [AntDesignVueResolver()],
+      globalComponentsDeclaration: true
+    }),
+  ],
+  alias: { // 别名
+    "@": path.resolve(__dirname, "src"), 
+    "views": path.resolve(__dirname, "src/views")
+  },
+  server: {
+    host: '0.0.0.0' // 新增内容
+  }
 })
